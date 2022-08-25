422a6a666298e6c4e482f7cbe9f92179afbab69c
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2022-08-25 23:32:23 +0600

    Part1-Course Information

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,38 @@
+import Header from './components/Header';
+import Total from './components/Total';
+import Content from './components/Content';
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      },
+    ],
+  };
+
+  return (
+    <div>
+        <h1>
+          <Header course={course.name}/>
+        </h1>
+        <h3>
+          <Content parts={course.parts} /><br/>
+          <Total parts={course.parts}/>
+        </h3>
+        
+    </div>
+  )
+}
+
+export default App
